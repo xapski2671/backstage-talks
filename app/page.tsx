66a19f9f62012a5a23e3bc9422308c7ae1a9ce13
xapski2671 <xapski2671@gmail.com>
2023-11-14 19:21:37 +0100

    scroll triggers

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,25 @@
+"use client"
 import Info from "@/containers/info"
 import s from "./page.module.sass"
 import Issue from "@/containers/issue"
+import issues from "../constants/issues.json"
+import { useState } from "react"
 
-export default function page() {
+export default function Page() {
+	const [color, setColor] = useState("#E30512")
+
+	function changeColor(index: number) {
+		setColor(issues[index].color)
+	}
+
 	return (
-		<div className={s.home}>
+		<div className={s.home} style={{ background: `${color}` }}>
 			<Info />
-			<Issue />
+			<Issue
+				currPosition={(idx: number) => {
+					changeColor(idx)
+				}}
+			/>
 		</div>
 	)
 }
--- containers/issue/index.tsx
+++ containers/issue/index.tsx
@@ -1,12 +1,55 @@
+"use client"
 import s from "./issue.module.sass"
+import issues from "../../constants/issues.json"
+import { useScroll } from "@/hooks/useScroll"
+import { useEffect, useState } from "react"
 
-export default function Issue() {
+export default function Issue({ currPosition }: { currPosition: Function }) {
+	const { scrollDirection } = useScroll()
+	const [position, setPosition] = useState(0)
+	const [callable, setCallable] = useState(true)
+
+	function changeIssue(e: any) {
+		if (e.deltaY < 0 && !(position > 6)) {
+			setPosition((prev) => prev + 1)
+		} else {
+			setPosition((prev) => prev - 1)
+		}
+	}
+
+	function trigger(e: any) {
+		if (callable) {
+			changeIssue(e)
+		}
+	}
+
+	useEffect(() => {
+		setCallable(false)
+		currPosition(position)
+		const tID = setTimeout(() => {
+			setCallable(true)
+		}, 2000)
+
+		return () => {
+			clearTimeout(tID)
+		}
+	}, [position])
+
 	return (
-		<div className={s.issue}>
-			<img src="/issue_1.png" alt="issue" />
-			<p>{"Issue #5"}</p>
-			<p>{"BUY HERE"}</p>
-			<p>{"or in selected stores"}</p>
+		<div
+			className={s.issue}
+			onWheel={(e) => {
+				trigger(e)
+			}}
+		>
+			<img
+				src={issues[!(position < 0) && !(position > 6) ? position : 0].src}
+				alt="issue"
+				className={s.issue_img}
+			/>
+			<p className={s.issue_no}>{`Issue #${position + 1}`}</p>
+			<p className={s.buy_info}>{"BUY HERE"}</p>
+			<p className={s.store_info}>{"or in selected stores"}</p>
 		</div>
 	)
 }
