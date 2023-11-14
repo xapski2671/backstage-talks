44562567b0e471a86469abd05c2494da6a7b2262
xapski2671 <xapski2671@gmail.com>
2023-11-14 15:19:21 +0100

    started issue design

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Info from "@/containers/info"
 import s from "./page.module.sass"
+import Issue from "@/containers/issue"
 
 export default function page() {
 	return (
 		<div className={s.home}>
 			<Info />
+			<Issue />
 		</div>
 	)
 }
--- containers/issue/index.tsx
+++ containers/issue/index.tsx
@@ -1,5 +1,12 @@
 import s from "./issue.module.sass"
 
-export default function index() {
-	return <div>index</div>
+export default function Issue() {
+	return (
+		<div className={s.issue}>
+			<img src="/issue_1.png" alt="issue" />
+			<p>{"Issue #5"}</p>
+			<p>{"BUY HERE"}</p>
+			<p>{"or in selected stores"}</p>
+		</div>
+	)
 }
